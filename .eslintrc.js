56ad6d3de05ef505a1683ed11924106604774188
jordancason@gmail.com <jordancason@gmail.com>
2018-08-14 13:44:38 -0400

    temp rule added

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
         "indent": ["error", 4],
         "react/prop-types": 0,
         "space-before-function-paren": 0,
+        "handle-callback-err": 0,
     },
     "parser": "babel-eslint"
 };
