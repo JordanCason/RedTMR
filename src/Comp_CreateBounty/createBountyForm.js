f8d986daf21db009654a76e89fc1dedceb9af7bd
jordancason@gmail.com <jordancason@gmail.com>
2018-09-01 00:18:46 -0400

    restructure moved some functions to redux actions

--- src/Comp_CreateBounty/createBountyForm.js
+++ src/Comp_CreateBounty/createBountyForm.js
@@ -2,53 +2,8 @@ import { Form, Input, Button, InputNumber } from 'antd'
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import Markdown from '../Comp_Markdown/Markdown'
-import faker from 'faker'
-import {profilePictureErrAction, profilePictureAction, setPictureLoadedTrueAction} from '../redux_actions/action_profilePicture.js'
-import {ipfsHashToast, transactionReceiptToast, transactionHashToast} from '../Comp_toast/Toast'
-import { ipfs } from '../Comp_IPFS/Ipfs'
-import {myContract, web3, bountyabi} from '../Comp_web3/abi.js'
-
+import {generateFormDataAction, profilePictureErrAction, profilePictureAction, setPictureLoadedTrueAction, submitBountyAction} from '../redux_actions/action_createBounty'
 const FormItem = Form.Item
-// const Option = Select.Option
-
-const submitBounty = (values, wallet) => {
-    console.log(values)
-    const { walletAddress } = wallet
-
-    // Need to addin a check to make sure values are enterd """Was getting an errer when no imag was upladed"""
-    ipfs.addJSON({ // push data to ipfs
-        uploadImg: values.uploadImg,
-        comName: values.comName,
-        comAbout: values.comAbout,
-        secEmail: values.secEmail,
-        maxEth: values.maxEth,
-        minEth: values.minEth,
-        Bountytextarea: values.Bountytextarea
-    }, (err, result) => {
-        ipfs.base58ToHex(result).then((result) => {
-            ipfsHashToast(result) // display IPFS hash in toast
-            console.log(values.deposit)
-            const deposit = web3.utils.toWei(values.deposit.toString(), 'ether')
-            // neet to use a try catch incase ipfs is down and send to error page
-            myContract.methods.createBounty(result).send({from: walletAddress, gas: 3000000, value: deposit}) // save ipfs hash and create contract
-                // neet to use a try catch incase ipfs is down and send to error page
-                .on('transactionHash', function(hash) {
-                    transactionHashToast(hash)
-                })
-                .on('receipt', (receipt) => {
-                    transactionReceiptToast(receipt, deposit)
-                    let bountyContractAddress = receipt.events.returnBounty.returnValues[0]
-                    let bountyContract = new web3.eth.Contract(bountyabi, bountyContractAddress)
-                    bountyContract.methods.ownerInfo().call().then(function(result) {
-                        console.log(`contract info: `, result)
-                    })
-                    // need to setup a listner to get contract address and display toast
-                })
-        })
-    })
-}
-
-
 const markdownInitCreateBounty = `
 ![Add an img](https://oracletimes.com/wp-content/uploads/2018/03/Ethereum-Cover.png "ETH")
 
@@ -59,9 +14,6 @@ No technology is perfect, and N/A believes that working with skilled security re
 * Let us know as soon as possible upon discovery of a potential security issue, and we'll make every effort to quickly resolve the issue.
 * Provide us a reasonable amount of time to resolve the issue before any disclosure to the public or a third-party.
 * Make a good faith effort to avoid privacy violations, destruction of data, and interruption or degradation of our service. Only interact with accounts you own or with explicit permission of the account holder.
-
-
-
 
 # Exclusions
 While researching, we'd like to ask you to refrain from:
@@ -90,49 +42,33 @@ React.render(
 | Minimum |10 ETH                      |5 ETH                  |2 ETH                    |0.1 ETH               |
 | Maximum |15 ETH                      |8 ETH                  |5 ETH                    |2 ETH                 |
 
-
-
-this is a link to [google][]
-
-[google]: http://www.google.com
-
-
-
 `
 
-
-
-
 class CreateBountyForm extends Component {
-  state = {
-      confirmDirty: false
-  };
+    state = {
+        confirmDirty: false
+    }
 
-  handleConfirmBlur = (e) => {
-      const value = e.target.value
-      this.setState({ confirmDirty: this.state.confirmDirty || !!value })
-  }
+    handleConfirmBlur = (e) => {
+        const value = e.target.value
+        this.setState({ confirmDirty: this.state.confirmDirty || !!value })
+    }
 
-  onChange = (value) => {
-      console.log('changed', value)
-  }
+    onChange = (value) => {
+        console.log('changed', value)
+    }
+
     generateFormData = () => {
-        this.props.setPictureLoadedTrueAction(faker.image.avatar())
-        this.props.form.setFields({
-            comName: {value: faker.company.companyName()},
-            website: {value: faker.internet.domainName()},
-            comAbout: {value: faker.company.catchPhrase()},
-            secEmail: {value: faker.internet.email()},
-            maxEth: {value: faker.random.number({min: 4, max: 13})},
-            minEth: {value: faker.random.number({min: 1, max: 3})}
+        this.props.generateFormDataAction().then(() => {
+            const formFields = this.props.createBounty.formFields
+            this.props.form.setFields(formFields)
         })
-    };
-    // uploadImg: uploadImg, comName: comName, comAbout: comAbout, secEmail: secEmail, maxEth: maxEth, minEth: minEth, Bountytextarea: Bountytextarea
+    }
+
     handleSubmit = (e) => {
         e.preventDefault()
         this.props.form.validateFieldsAndScroll((err, values) => {
-            console.log(this.props.profilePicture.loaded)
-            if (!this.props.profilePicture.loaded) {
+            if (!this.props.createBounty.loaded) {
                 if (!err) {
                     err = {}
                 }
@@ -140,18 +76,9 @@ class CreateBountyForm extends Component {
                 this.props.profilePictureErrAction(err.picture)
             }
             if (!err) {
-                // const test = ReactMarkdown({
-                //     source: this.props.markdown.createBounty,
-                //     rawSourcePos: true
-                // })
-                values.uploadImg = this.props.profilePicture.picture
-
-                console.log(this.props.profilePicture.picture)
+                values.uploadImg = this.props.createBounty.picture
                 values.Bountytextarea = this.props.markdown.createBounty
-                // console.log(this.props.profilePicture)
-                // console.log('Received values of form: ', values);
-                submitBounty(values, this.props.ethereumWallet)
-                // submit data to ipfs and ethereum
+                this.props.submitBountyAction(values, this.props.ethereumWallet)
             }
         })
     }
@@ -169,6 +96,7 @@ class CreateBountyForm extends Component {
                 sm: { span: 8 }
             }
         }
+
         const tailFormItemLayout = {
             wrapperCol: {
                 xs: {
@@ -181,7 +109,6 @@ class CreateBountyForm extends Component {
                 }
             }
         }
-
 
         return (
             <Form onSubmit={this.handleSubmit}>
@@ -232,14 +159,17 @@ class CreateBountyForm extends Component {
 const mapStateToProps = state => ({
     profilePicture: state.profilePicture,
     markdown: state.markdown,
-    ethereumWallet: state.ethereumWallet
+    ethereumWallet: state.ethereumWallet,
+    createBounty: state.createBounty
 
 })
 
 const mapActionsToProps = {
     profilePictureAction,
     profilePictureErrAction,
-    setPictureLoadedTrueAction
+    setPictureLoadedTrueAction,
+    generateFormDataAction,
+    submitBountyAction
 
 }
 
